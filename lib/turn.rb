ce8793e1f88c479b367a6a5cb13d94cb67a17fca
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-03 14:26:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,12 +2,14 @@ def input_to_index(move)
    index = move.to_i - 1
    index
 end
+
  def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+
  def valid_move?(board, index)
-   if index.between?(0,8) && !position_taken?(board, index)
-      puts 'this is a valid move'
+   if index.between?(0, 8) && !position_taken?(board, index)
+      puts "This is a valid move"
     return true
   else
    return false
