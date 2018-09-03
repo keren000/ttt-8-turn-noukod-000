b7e12b5e5fa698a2927addb4edf4641e56d28c05
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-03 14:42:17 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,69 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+def input_to_index(move)
+   index = move.to_i - 1
+   index
+end
+
+ def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+ def valid_move?(board, index)
+   if index.between?(0, 8) && !position_taken?(board, index)
+      puts "This is a valid move"
+    return true
+  else
+   return false
+  end
+end
+
+def move(board, index, token = "X")
+  board[index] = token
+  return(board)
+ end
+
+ def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+
+  #input to index
+  index = input_to_index(user_input)
+  token = current_player(board)
+
+   #check for validation
+  if valid_move?(board,index)
+    puts "Valid move"
+    move(board, index, token)
+    display_board(board)
+   else
+    puts "Try again"
+    turn(board)
+  end
+  display_board(board)
+end
+
+ def current_player(board)
+  if turn_count(board)%2 ==0
+    current_player = "X"
+  else
+    current_player = "O"
+end
+return current_player
+end
+ def turn_count(board)
+  turncounter = 0
+  board.each do |index|
+    if index == "X" || index == "O"
+      turncounter +=1
+  end
+end
+return turncounter
+end
