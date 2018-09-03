20259259ec9222f9c94736841febaa35a58cfacd
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-03 14:36:34 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,51 +1,59 @@
+def input_to_index(move)
+   index = move.to_i - 1
+   index
+end
 
- def display_board
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
- def input_to_index(user_input)
-  user_input.to_i - 1
-end
- def move(index, current_player = "X")
-  board[index] = current_player
-end
- def position_taken?(index)
+ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
- def valid_move?(index)
-  index.between?(0,8) && !position_taken?(index)
-end
- def turn_count
-  turn = 0
-  board.each do |index|
-    if index == "X" || index == "O"
-      turn += 1
-    end
-  end
-  return turn
-end
- def current_player
-  #if the turn count is an even number, that means O just went, so the next/current player is X
-  num_turns = turn_count
-  if num_turns % 2 == 0
-    player = "X"
+
+ def valid_move?(board, index)
+   if index.between?(0, 8) && !position_taken?(board, index)
+      puts "This is a valid move"
+    return true
   else
-    player = "O"
+   return false
   end
-  return player
 end
- def turn
-  puts "Please choose a number 1-9:"
-  user_input = gets.chomp
+
+def move(board, index, token = "X")
+  board[index] = token
+  return(board)
+ end
+
+ def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+
+  #input to index
   index = input_to_index(user_input)
-  if valid_move?(index)
-    player_token = current_player
-    move(index, player_token)
-    display_board
-  else
-    turn
+  token = current_player(board)
+
+   #check for validation
+  if valid_move?(board,index)
+    puts "Valid move"
+    move(board, index, token)
+    display_board(board)
+   else
+    puts "Try again"
+    turn(board)
   end
+  display_board(board)
+end
+ def current_player(board)
+  if turn_count(board)%2 ==0
+    current_player = "X"
+  else
+    current_player = "O"
+end
+return current_player
+end
+ def turn_count(board)
+  turncounter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turncounter +=1
+  end
+end
+return turncounter
 end
