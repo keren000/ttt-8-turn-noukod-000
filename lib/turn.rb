7e492e2814dca7b19e16ca4b724961c2c8ddf5c7
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-03 14:33:57 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,21 +1,19 @@
-def initialize
-  @board = Array.new(9, " ")
-end
+
  def display_board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
  def input_to_index(user_input)
   user_input.to_i - 1
 end
  def move(index, current_player = "X")
-  @board[index] = current_player
+  board[index] = current_player
 end
  def position_taken?(index)
-  !(@board[index].nil? || @board[index] == " ")
+  !(board[index].nil? || board[index] == " ")
 end
  def valid_move?(index)
   index.between?(0,8) && !position_taken?(index)
@@ -51,4 +49,3 @@ end
     turn
   end
 end
- 
